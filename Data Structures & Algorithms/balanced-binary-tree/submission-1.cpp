22cbc759a8ecb279e02f4939c4067a2447f6d26d
P L V Sai Pawan Kumar <47073752+plvsaipawankumar@users.noreply.github.com>
2026-06-20 13:41:40 +0530

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int balancedTree(TreeNode* root, bool & ans)
+    {
+        if(root==nullptr )
+            return 0;
+        int left = balancedTree(root->left,ans);
+        int right = balancedTree(root->right,ans);
+        ans = ans && (abs(left-right)<2);
+        cout<<"root->val :"<<root->val<<" left : "<<left<<" right : "<<right<<" ans :"<<ans<<endl;
+        return max(left,right)+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root==nullptr)return true;
+        bool ans=true;
+        int x= balancedTree(root,ans);
+        return ans;
+    }
+};
